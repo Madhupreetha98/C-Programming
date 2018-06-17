efcd1bab35a6eca6969889db2fe53ac0eebe0fb8
Madhupreetha98 <36624004+Madhupreetha98@users.noreply.github.com>
2018-06-17 12:34:37 +0530

    revnum.c

---
+++ player/revnum.c
@@ -0,0 +1,18 @@
+ #include<stdio.h>
+ #include<conio.h>
+ int main()
+ {
+ int n,i,rem,rev=0;
+ scanf("%d",&n);
+ while(n!=0)
+
+
+   { rem=n%10;
+
+      rev = rem+rev*10;
+      n       = n/10;
+   }
+
+ printf("%d",rev);
+ return 0;
+ }
